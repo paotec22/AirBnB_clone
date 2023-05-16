959e4edd1989e95f45bb421faee9701dc2a3a921
Paul Sobande <paotec22@gmail.com>
2023-05-16 02:40:33 +0100

    models_files

---
+++ one/models/amenity.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+"""module creates an Amenity class"""
+
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """Class will manage amenity objects"""
+
+    name = ""
